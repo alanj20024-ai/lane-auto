42514fa751a5619275ec4e081b581290a4c563b8
Alan <alanj20024@gmail.com>
2026-07-07 11:16:51 -0500

    Add Alpha Hotshot landing site

---
+++ outputs/alpha-hotshot-improved/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <p>
+        If the landing page does not load automatically, open{" "}
+        <a href="/index.html">Alpha Hotshot</a>.
+      </p>
+    </main>
+  );
+}
